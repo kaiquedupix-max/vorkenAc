7f2f99cd6397e2a57ad5cad1dcb99d0b7a3518e0
kaiquedupix-max <kaiquedupix@gmail.com>
2026-09-24 19:10:16 -0300

    fix: add regex namespace for USB identity matching

--- agent/Program.cs
+++ agent/Program.cs
@@ -7,6 +7,7 @@ using System.Net.Http.Json;
 using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Text.Json.Serialization;
